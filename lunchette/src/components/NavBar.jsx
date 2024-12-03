0ecda3d431d1cf3ae3b65eec6adca62c2d299c80
youssef benameur <ybenameur66@gmail.com>
2024-12-03 15:42:34 +0100

    d

--- lunchette/src/components/NavBar.jsx
+++ lunchette/src/components/NavBar.jsx
@@ -2,8 +2,7 @@ import { IonIcon } from "@ionic/react";
 import { menuOutline, personOutline, helpCircleOutline } from "ionicons/icons";
 const NavBar = () => {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     <>
       <header className="w-screen h-20 shadow-md flex items-center space-x-4 fixed top-0 left-0 bg-white z-50 ">
         <div className="h-full  flex items-center w-96 hover:bg-gray-100 duration-200  ">
@@ -31,36 +30,7 @@ const NavBar = () => {
       </header>
       <SideBar isCollapsed={isCollapsed}></SideBar>
     </>
-=======
-=======
->>>>>>> parent of 3a8c8d2 (navbar + sidebar)
-    <header className="w-screen h-20 shadow-md flex items-center space-x-4 ">
-      <div className="h-full flex-1 flex items-center hover:bg-gray-100 duration-200  ">
-        <IonIcon
-          icon={menuOutline}
-          style={{ fontSize: "40px", color: "black" }}
-        />
-        <p className="text-3xl font-medium mb-1 font-mono ml-6">Home</p>
-      </div>
-      <div className=" flex-grow flex justify-center items-center text-3xl  font-bold text-rose-300  ">
-        <p>Lunchette</p>
-      </div>
-      <div className="flex-1 justify-end flex items-center  space-x-8 ">
-        <IonIcon
-          icon={personOutline}
-          style={{ fontSize: "30px", color: "black" }}
-        ></IonIcon>
-        <IonIcon
-          className=" "
-          icon={helpCircleOutline}
-          style={{ fontSize: "30px", color: "black" }}
-        ></IonIcon>
-      </div>
-    </header>
-<<<<<<< HEAD
->>>>>>> parent of 3a8c8d2 (navbar + sidebar)
-=======
->>>>>>> parent of 3a8c8d2 (navbar + sidebar)
+
   );
 };
 export default NavBar;
