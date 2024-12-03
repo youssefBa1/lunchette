e53e7abb2c5639505b8685812138515e80f63ecf
youssef benameur <ybenameur66@gmail.com>
2024-12-03 15:35:21 +0100

    d

--- lunchette/src/App.jsx
+++ lunchette/src/App.jsx
@@ -1,7 +1,17 @@
 import NavBar from "./components/navbar";
+<<<<<<< HEAD
 import AgendaView from "./views/AgendaView";
 function App() {
   return <AgendaView></AgendaView>;
+=======
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+>>>>>>> parent of 3a8c8d2 (navbar + sidebar)
 }
 
 export default App;
--- lunchette/vite.config.js
+++ lunchette/vite.config.js
@@ -1,9 +1,19 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+>>>>>>> parent of 3a8c8d2 (navbar + sidebar)
 
 export default defineConfig({
+<<<<<<< HEAD
   resolve: {
     alias: {
       "@": "/src", // This sets up the alias for your source folder
     },
   },
 });
+=======
+  plugins: [react()],
+})
+>>>>>>> parent of 3a8c8d2 (navbar + sidebar)
